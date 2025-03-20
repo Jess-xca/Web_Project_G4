d2897cb379104b4cba189f8f009388729f462f54
dondou21 <dondouabiyi@gmail.com>
2025-03-20 12:25:20 +0200

    Red shape added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+<<<<<<< Updated upstream
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+=======
+<<<<<<< Updated upstream
+  content: ["./src/**/*.{html,js}"],
+=======
+   
+content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   theme: {
     extend: {},
   },
