80897f17ff3c562c6b099df689542cfd266ad315
dondou21 <dondouabiyi@gmail.com>
2025-03-20 15:39:18 +0200

    responsive design updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xll': '1040px' ,
+        'xxl': '1548px', 
+        '2xxl' : '2560px',
+      },
       colors: {
         primary: "#9747FF",
       },
