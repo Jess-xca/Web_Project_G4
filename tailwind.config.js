515392a7f197193e47141094c1902e9b4028e378
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-03-24 16:33:07 +0200

    Updated the contact.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/sections/*.html"],
   theme: {
     extend: {
       colors: {
         primary: "#9747FF",
         secondary: "#00672E",
-        tertiary: "#FFE800",
       },
     },
   },
