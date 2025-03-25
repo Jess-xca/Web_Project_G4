b11c975c21b1d49bc1b2ecce83e6bbc1f7571941
andre <andy49miguel@gmail.com>
2025-03-25 19:50:10 +0200

    adding sections to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.html",
+    "./src/sections/*.html"
+  ],
   theme: {
     extend: {
       colors: {
