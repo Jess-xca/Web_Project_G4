e36628777af64b4279606c579a4b8f0056b199b7
andre <andy49miguel@gmail.com>
2025-03-25 12:25:18 +0200

    chore: tablet responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    "./src/*.html",
+  ],
   theme: {
     extend: {
       colors: {
