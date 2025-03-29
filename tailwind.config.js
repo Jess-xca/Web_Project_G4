86adfc943331cabadb165aefc4f7656fec502c0c
andre <andy49miguel@gmail.com>
2025-03-29 08:11:57 +0200

    updated project format & code to match format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.html",
-    "./src/sections/*.html"
-  ],
+  content: ["./*.html","./src/sections/*.html"],
   theme: {
     extend: {
       colors: {
-        primary: "#9747FF",
         base: "#EFECEA",
-        green: "#00672E",
       },
       fontFamily: {
-        custom: ["Urbanist", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
       },
     },
   },
