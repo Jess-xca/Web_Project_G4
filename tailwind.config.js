f530435124c7ece0c25d2f883bbe63e7876566e8
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-04-02 12:58:38 +0200

    Removed purple color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
 export default {
-  content: ["./*.html","./src/sections/*.html"],
+  content: ["./*.html", "./src/sections/*.html"],
   theme: {
     extend: {
-      screens: {
-        'xxl': '1548px', 
-      },
       colors: {
-        yellow: "#FFE800",
         base: "#EFECEA",
         green: "#00672E",
-        purple: "#9747FF"
       },
       fontFamily: {
-        outfit: ['Outfit', 'sans-serif']
-      }
+        outfit: ["Outfit", "sans-serif"],
+      },
     },
   },
   plugins: [],
