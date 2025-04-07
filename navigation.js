99bf803b1d7825e776668130aa9df4c68822c447
andre <andy49miguel@gmail.com>
2025-04-07 14:18:49 +0200

    chore: index & navigation script initial setup done

--- navigation.js
+++ navigation.js
@@ -1,10 +1,3 @@
-// const loadComponents = (id, url) => {
-//   return fetch(url)
-//     .then((response) => response.text())
-//     .then((html) => {
-//       document.getElementById(id).innerHTML = html;
-//     });
-// };
 const loadComponents = async (id, url) => {
   const response = await fetch(url);
   const html = await response.text();
@@ -12,12 +5,16 @@ const loadComponents = async (id, url) => {
 };
 window.onload = _ => {
   Promise.all([
+  loadComponents("mobile-menu", "/src/sections/mobile_menu.html"),
+  loadComponents("custom_mobile-menu", "src/sections/custom_mobile_menu.html"),
   loadComponents("header", "src/sections/header.html"),
   loadComponents("hero", "src/sections/hero.html"),
   loadComponents("about", "src/sections/about.html"),
   loadComponents("bs", "src/sections/bs.html"),
   loadComponents("reference", "src/sections/reference.html"),
-  loadComponents("footer", "src/sections/footer.html")
+  loadComponents("footer", "src/sections/footer.html"),
+  loadComponents("imprint", "src/sections/imprint-page.html"),
+  loadComponents("privacy-pol", "src/sections/privacy_pol-page.html")
   ]).then( _ => {
     document.getElementById("menu-a").addEventListener("click", _ => console.log("clicked!"))
   });
