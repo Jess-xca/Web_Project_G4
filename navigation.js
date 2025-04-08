9e40fa3ef5cd3718b4466bbff48c1781a455d1f4
andre <andy49miguel@gmail.com>
2025-04-08 20:54:42 +0200

    chore: more work including reference matching

--- navigation.js
+++ navigation.js
@@ -5,17 +5,37 @@ const loadComponents = async (id, url) => {
 };
 window.onload = _ => {
   Promise.all([
-  loadComponents("mobile-menu", "/src/sections/mobile_menu.html"),
-  loadComponents("custom_mobile-menu", "src/sections/custom_mobile_menu.html"),
-  loadComponents("header", "src/sections/header.html"),
-  loadComponents("hero", "src/sections/hero.html"),
-  loadComponents("about", "src/sections/about.html"),
-  loadComponents("bs", "src/sections/bs.html"),
-  loadComponents("reference", "src/sections/reference.html"),
-  loadComponents("footer", "src/sections/footer.html"),
-  loadComponents("imprint", "src/sections/imprint-page.html"),
-  loadComponents("privacy-pol", "src/sections/privacy_pol-page.html")
+    loadComponents("mobile-menu", "/src/sections/mobile_menu.html"),
+    loadComponents("custom_mobile-menu", "src/sections/custom_mobile_menu.html"),
+    loadComponents("header", "src/sections/header.html"),
+    loadComponents("hero", "src/sections/hero.html"),
+    loadComponents("about", "src/sections/about.html"),
+    loadComponents("bs", "src/sections/bs.html"),
+    loadComponents("reference", "src/sections/reference.html"),
+    loadComponents("footer", "src/sections/footer.html"),
   ]).then( _ => {
-    document.getElementById("menu-a").addEventListener("click", _ => console.log("clicked!"))
+    const menu = document.getElementById("mobile-menu");
+    // const cMenu = document.getElementById("custom_mobile-menu");
+    const main = document.getElementById("home");
+    document.getElementById("footer-imprintS").addEventListener("click", _ => {
+      _.preventDefault();
+
+      window.location.href = "/src/sections/privacy_pol-page.html";
+      window.addEventListener("DOMContentLoaded", _ => {
+        document.getElementById("imprint").classList.toggle("hidden");
+      })
+    })
+    document.getElementById("phone-menu").addEventListener("click", _ => {
+      main.classList.toggle("hidden");
+      menu.classList.toggle("hidden");
+    })
+    document.getElementById("xphone-menu").addEventListener("click", _ => {
+      main.classList.toggle("hidden");
+      menu.classList.toggle("hidden");
+    })
+    document.getElementById("mm-links").addEventListener("click", _ => {
+      main.classList.toggle("hidden");
+      menu.classList.toggle("hidden");
+    })
   });
 };
